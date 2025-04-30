ce145948cd15116043febcf10910153b5828cb03
SavioMariano <saviomariano77@gmail.com>
2025-04-30 14:31:47 -0300

    modificando classe livro

--- src/main/java/com/list/library/domain/model/Livro.java
+++ src/main/java/com/list/library/domain/model/Livro.java
@@ -2,6 +2,8 @@ package com.list.library.domain.model;
 
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+
 @Entity(name = "tb_Livro")
 public class Livro {
     
@@ -10,8 +12,14 @@ public class Livro {
     private Long id;
 
     @Column
-    private 
+    private String genero;
 
+    @Column
     private String autor;
+
+    // scale é a quantidade de numeros pós virgula e precision 13 o total de numeros
+    // Ex: 99.999.999.999,99
+    @Column(precision = 13, scale = 2)
+    private BigDecimal preco;
 
 }
